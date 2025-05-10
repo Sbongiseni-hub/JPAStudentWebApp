2d4b92496959a264dee1dcdf26cb6b876f53b107
Sbongiseni Mdau <sbongisenimdau@outlook.com>
2025-05-10 20:38:13 +0200

    commited

--- WorkersEJBModule/src/java/za/ac/tut/model/entity/Worker.java
+++ WorkersEJBModule/src/java/za/ac/tut/model/entity/Worker.java
@@ -6,13 +6,10 @@
 package za.ac.tut.model.entity;
 
 import java.io.Serializable;
-import java.util.Date;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Temporal;
 
 /**
  *
@@ -24,72 +21,15 @@ public class Worker implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "ID_NUMBER")
     private Long id;
-   
-    
-    @Column(name = "NAME")
-    private String name;
-    
-    @Column(name = "WORKSER_AGE")
-    private Integer age;
-    
-    @Temporal(javax.persistence.TemporalType.DATE)
-   @Column(name = "BIRTH_DATE")
-   private Date dob;
 
-    public Worker() {
-    }
-
-
-    public Worker(Long id, String name, Integer age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
-    
-    
-    
     public Long getId() {
         return id;
     }
 
-    public static long getSerialVersionUID() {
-        return serialVersionUID;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    
-    public Integer getAge() {
-        return age;
-    }
-
-    public Date getDob() {
-        return dob;
-    }
-    
-    
-    
-
     public void setId(Long id) {
         this.id = id;
     }
-
-    public void setDob(Date dob) {
-        this.dob = dob;
-    }
-    
 
     @Override
     public int hashCode() {
